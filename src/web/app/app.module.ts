6b719d201db9eee17e1278aa902a97005af666e7
Ilyar <761285+ilyar@users.noreply.github.com>
2021-09-18 09:36:56 +0300

    init 2

--- src/web/app/app.module.ts
+++ src/web/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {ServiceWorkerModule} from '@angular/service-worker'
+import {environment} from '../environments/environment'
+import {WelcomeComponent} from './welcome/welcome.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
